7f742fa25811e351dfc6a291c02865a8b51b7b1c
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-03-17 21:10:58 -0700

    Assignment 3: Improve style

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-2!114#us^_^)b5%r$_(h9dd6e3$d620!p#)pfr%d_b9=kxsx!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = ['192.168.1.123', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
